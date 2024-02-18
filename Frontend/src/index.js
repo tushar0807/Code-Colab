92a7485d826600a1e23f6c7c31dacd22a8313360
tushar0807 <135457535+tushar0807@users.noreply.github.com>
2024-02-18 17:16:24 +0530

    minor change

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -11,7 +11,7 @@ import {
 import { StoreProvider } from './assets/StoreContext';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://code-colab-tvz6.onrender.com',
   cache: new InMemoryCache(),
 })
 
